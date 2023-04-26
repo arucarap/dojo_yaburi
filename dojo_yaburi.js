2758b05dff74c9f0e5932abf868fa0b48f8e688a
arucarap <thumb.jpg.exe@gmail.com>
2023-04-26 15:29:08 +0900

    [add] 過去問道場のアドブロック対策を無効化する拡張機能を追加

---
+++ dojo_yaburi.js
@@ -0,0 +1,24 @@
+var coverClassName = '';
+const hostname = new URL(window.location.href).hostname;
+if(/fp.-siken/.test(hostname)) {
+  coverClassName = 'kaisetsu';
+} else {
+  coverClassName = 'ansbg';
+}
+
+const covers = document.getElementsByClassName(coverClassName);
+
+const observer = new MutationObserver(_records => {
+  for (var i = 0; i < covers.length; i++) {
+    covers[i].style.filter = null;
+    covers[i].style.opacity = null;
+    covers[i].style.userSelect = null;
+  }
+});
+
+for (var i = 0; i < covers.length; i++) {
+  observer.observe(covers[i], {
+    attributes: true,
+    attributeFilter: ['style']
+  });
+}
